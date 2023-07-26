b0c71f9f431117398a43db42f26b642ca77c904d
Kail Keusch <kfkkail@gmail.com>
2023-07-26 13:59:05 -0400

    Import Record

--- src/structure/immutable/getIn.js
+++ src/structure/immutable/getIn.js
@@ -1,10 +1,8 @@
 /* Code from github.com/erikras/redux-form by Erik Rasmussen */
-import { Iterable } from 'immutable'
-import plainGetIn from '../plain/getIn'
+import { Iterable, Record } from "immutable"
+import plainGetIn from "../plain/getIn"
 
 const getIn = (state, path) =>
-  Iterable.isIterable(state) || Record.isRecord(value)
-    ? state.getIn(path)
-    : plainGetIn(state, path)
+  Iterable.isIterable(state) || Record.isRecord(state) ? state.getIn(path) : plainGetIn(state, path)
 
 export default getIn
--- src/structure/immutable/index.js
+++ src/structure/immutable/index.js
@@ -1,11 +1,11 @@
-import { Iterable, fromJS } from 'immutable'
-import getIn from './getIn'
+import { Iterable, fromJS, Record } from "immutable"
+import getIn from "./getIn"
 
 const structure = {
   fromJS: fromJS,
   getIn,
   merge: (state, payload) => state.merge(payload),
-  toJS: value => Iterable.isIterable(value) || Record.isRecord(value) ? value.toJS() : value,
+  toJS: (value) => (Iterable.isIterable(value) || Record.isRecord(value) ? value.toJS() : value),
 }
 
 export default structure
