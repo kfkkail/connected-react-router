95a59abc71f3898ef0d72bf56e03abd3811d8bb2
Kail Keusch <kfkkail@gmail.com>
2023-07-26 14:49:56 -0400

    Remove console.logs

--- src/selectors.js
+++ src/selectors.js
@@ -10,11 +10,6 @@ const createSelectors = (structure) => {
 
   const getRouter = state => {
     const router = toJS(getIn(state, ['router']))
-    console.log("connected-reat-router check", router, state)
-    console.log("crr type", typeof router)
-    console.log("crr location", getIn(router, ['location']) || "undefined")
-    console.log("crr action", getIn(router, ["action"]) || "undefined")
-    console.log("check", !isRouter(router))
     if (!isRouter(router)) { throw 'Could not find router reducer in state tree, it must be mounted under "router"' }
     return router
   }
--- src/structure/immutable/getIn.js
+++ src/structure/immutable/getIn.js
@@ -2,11 +2,7 @@
 import { Iterable, Record } from "immutable"
 import plainGetIn from "../plain/getIn"
 
-const getIn = (state, path) => {
-const blah = Iterable.isIterable(state) || Record.isRecord(state) ? state.getIn(path) : plainGetIn(state, path)
-console.log("getIn", Iterable.isIterable(state) || Record.isRecord(state), state, path, blah);
-return blah
-}
-
+const getIn = (state, path) =>
+  Iterable.isIterable(state) || Record.isRecord(state) ? state.getIn(path) : plainGetIn(state, path)
 
 export default getIn
